6320ca721ff220b2ed8085792ce9541498adfd48
starlying <corp@siteserver.cn>
2018-01-19 16:38:33 +0800

    dev

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,8 +4,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("SiteServer.Plugin")]
 [assembly: AssemblyDescription("Reference this library if you want to develop a SiteServer CMS plugin.")]
-[assembly: AssemblyCopyright("Copyright © SiteServer CMS 2017")]
+[assembly: AssemblyCopyright("Copyright © SiteServer CMS 2018")]
 [assembly: Guid("c22be00d-a6f5-4e45-8ecc-09ebf297c812")]
-[assembly: AssemblyVersion("1.0.7")]
 [assembly: InternalsVisibleTo("SiteServer.CMS")]
 [assembly: InternalsVisibleTo("SiteServer.BackgroundPages")]
