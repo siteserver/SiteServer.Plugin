efca631914dd5419266f3bbfc8a89d5bafc067f6
starlying <corp@siteserver.cn>
2018-01-20 09:05:45 +0800

    version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,3 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: Guid("c22be00d-a6f5-4e45-8ecc-09ebf297c812")]
 [assembly: InternalsVisibleTo("SiteServer.CMS")]
 [assembly: InternalsVisibleTo("SiteServer.BackgroundPages")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
