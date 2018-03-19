aea7aa831d888ec3090c7b662567c38272629924
starlying <corp@siteserver.cn>
2018-03-19 09:40:50 +0800

    channel api

--- IChannelApi.cs
+++ IChannelApi.cs
@@ -8,6 +8,8 @@ namespace SiteServer.Plugin
 
         IChannelInfo GetChannelInfo(int siteId, int channelId);
 
+        string GetChannelName(int siteId, int channelId);
+
         List<int> GetChannelIdList(int siteId, string adminName);
 
         List<int> GetChannelIdList(int siteId);
@@ -15,5 +17,9 @@ namespace SiteServer.Plugin
         List<int> GetChannelIdList(int siteId, int parentId);
 
         int Insert(int siteId, IChannelInfo channelInfo);
+
+        void Update(int siteId, IChannelInfo channelInfo);
+
+        void Delete(int siteId, int channelId);
     }
 }
