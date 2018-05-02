3025b99a08d1ccc55d83f7df2cd0654e902138ae
starlying <starlying@gmail.com>
2018-05-03 07:06:29 +0800

    service

--- IService.cs
+++ IService.cs
@@ -6,9 +6,6 @@ namespace SiteServer.Plugin
 {
     public interface IService
     {
-        // 
-        // Action<object, FileSystemEventArgs> FileSystemChanged { get; }
-
         event EventHandler<ContentEventArgs> ContentAddCompleted;
 
         event EventHandler<ContentEventArgs> ContentDeleteCompleted;
@@ -18,8 +15,6 @@ namespace SiteServer.Plugin
         event EventHandler<ContentFormSubmitEventArgs> ContentFormSubmit;
 
         event ContentFormLoadEventHandler ContentFormLoad;
-
-        //IService AddContentForm(string attributeName, Func<int, int, IAttributes, string> customized);
 
         event EventHandler<ParseEventArgs> BeforeStlParse;
 
@@ -44,53 +39,5 @@ namespace SiteServer.Plugin
         event ApiEventHandler ApiPut;
 
         event ApiEventHandler ApiDelete;
-
-        //IService AddJsonGet(Func<IRequest, object> jsonGet);
-
-        //IService AddJsonGet(Func<IRequest, string, object> jsonGetWithName);
-
-        //IService AddJsonGet(Func<IRequest, string, string, object> jsonGetWithNameAndId);
-
-        //IService AddJsonPost(Func<IRequest, object> jsonPost);
-
-        //IService AddJsonPost(Func<IRequest, string, object> jsonPostWithName);
-
-        //IService AddJsonPost(Func<IRequest, string, string, object> jsonPostWithNameAndId);
-
-        //IService AddJsonPut(Func<IRequest, object> jsonPut);
-
-        //IService AddJsonPut(Func<IRequest, string, object> jsonPutWithName);
-
-        //IService AddJsonPut(Func<IRequest, string, string, object> jsonPutWithNameAndId);
-
-        //IService AddJsonDelete(Func<IRequest, object> jsonDelete);
-
-        //IService AddJsonDelete(Func<IRequest, string, object> jsonDeleteWithName);
-
-        //IService AddJsonDelete(Func<IRequest, string, string, object> jsonDeleteWithNameAndId);
-
-        //IService AddHttpGet(Func<IRequest, HttpResponseMessage> httpGet);
-
-        //IService AddHttpGet(Func<IRequest, string, HttpResponseMessage> httpGetWithName);
-
-        //IService AddHttpGet(Func<IRequest, string, string, HttpResponseMessage> httpGetWithNameAndId);
-
-        //IService AddHttpPost(Func<IRequest, HttpResponseMessage> httpPost);
-
-        //IService AddHttpPost(Func<IRequest, string, HttpResponseMessage> httpPostWithName);
-
-        //IService AddHttpPost(Func<IRequest, string, string, HttpResponseMessage> httpPostWithNameAndId);
-
-        //IService AddHttpPut(Func<IRequest, HttpResponseMessage> httpPut);
-
-        //IService AddHttpPut(Func<IRequest, string, HttpResponseMessage> httpPutWithName);
-
-        //IService AddHttpPut(Func<IRequest, string, string, HttpResponseMessage> httpPutWithNameAndId);
-
-        //IService AddHttpDelete(Func<IRequest, HttpResponseMessage> httpDelete);
-
-        //IService AddHttpDelete(Func<IRequest, string, HttpResponseMessage> httpDeleteWithName);
-
-        //IService AddHttpDelete(Func<IRequest, string, string, HttpResponseMessage> httpDeleteWithNameAndId);
     }
 }
