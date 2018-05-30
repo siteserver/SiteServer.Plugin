1325441bde8e2313b66c2a0f1efae2c3262472ce
starlying <starlying@gmail.com>
2018-05-30 14:32:10 +0800

    parse

--- IParseApi.cs
+++ IParseApi.cs
@@ -4,15 +4,11 @@ namespace SiteServer.Plugin
 {
     public interface IParseApi
     {
-        Dictionary<string, string> GetStlElements(string innerXml, List<string> stlElementNames);
+        Dictionary<string, string> GetStlElements(string html, List<string> stlElementNames);
 
-        string ParseInnerXml(string innerXml, IParseContext context);
+        string Parse(string html, IParseContext context);
 
         string ParseAttributeValue(string attributeValue, IParseContext context);
-
-        string HtmlToXml(string html);
-
-        string XmlToHtml(string xml);
 
         string GetCurrentUrl(IParseContext context);
     }
